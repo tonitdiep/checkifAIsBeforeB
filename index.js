e2971cf8140a039dbf680f5cd682a63bee227f78
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-01-05 18:29:38 +0000

    question

---
+++ index.js
@@ -0,0 +1,22 @@
+We are check if All A's Appears Before All B's. 
+
+Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
+
+
+Input: s = "aaabbb"
+Output: true
+Explanation:
+The 'a's are at indices 0, 1, and 2, while the 'b's are at indices 3, 4, and 5.
+Hence, every 'a' appears before every 'b' and we return true.
+
+Input: s = "abab"
+Output: false
+Explanation:
+There is an 'a' at index 2 and a 'b' at index 1.
+Hence, not every 'a' appears before every 'b' and we return false.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkString = function(s) {}
